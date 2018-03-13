0da0ac5a60d4ec205259097f4f5a09afe10a6574
Edward Lau <edward.lau@me.com>
2018-03-12 17:37:21 -0700

    removed docopt

--- riana.py
+++ riana.py
@@ -1,33 +1,10 @@
 """
-Relative Isotope Abundance Analyzer v.0.2.0. Build Date : : :.
+Relative Isotope Abundance Analyzer v.0.3.0. Build Date : : :.
 Written by Edward Lau (edward.lau@me.com) 2016-2018
 
 
-Usage:
-    riana.py --help
-    riana.py integrate <mzid> <mzml>  [--iso=0,6 --lys=0 --unique --test --qvalue=1e-2 --rt=1.0 --out=ria.txt ]
-    riana.py integrate_fast <mzid> <mzml>  [--iso=0,6 --lys=0 --unique --test --qvalue=1e-2 --rt=1.0 --out=ria.txt ]
-    riana.py merge <index>
-
-Options:
-    -h --help            Show this screen.
-    -v --version         Show version.
-    --iso=0,6            Isotopes to do separated by commas e.g., 0,1,2,3,4,5 [default: 0,6].
-    -u --unique          Integrate unique peptides only
-    -K --lys=0           Filter peptides with number of lysines (0=do not filter, 1=only one lysine,
-                         2=any number of lysines [default: 0]
-    -t --test            Integrate only the first 5 qualifying peptides for testing purpose
-    -q --qvalue=1e-2     Integrate only peptides/proteins with percolator q value below this threshold [default: 1e-2]
-    --rt=1.0             Retention time (in minutes, both directions), tolerance for integration
-    --out=ria.txt        Name of output file [default: ria.txt]
-
-Example:
-    riana.py integrate_fast percolator.target.mzid Heart_FASP_1.mzML --iso 0,6,12 -t -K 0 --qvalue 5e-3
-    riana.py merge ria_index.txt
-
 """
 
-from docopt import docopt
 from pymzid import Mzid
 from integrate_mzml import Mzml
 from time import time
@@ -45,202 +22,13 @@ import os
 # rt_tolerance=1
 # iso_to_do = [0,1,2,3,4,5,6]
 
+
+
 def integrate(args):
     """
-    DEPRECATED
-    Main loop for isotope abundance analysis
-
-    :param args: Arguments from command line
-    :return:
-
-    """
-
-    # Handle command line arguments
-    mzid_loc = args['<mzid>']
-    mzml_loc = args['<mzml>']
-
-    # Handle command line options
-    out_loc = args['--out']
-    test_run = args['--test']
-    unique_pep = args['--unique']
-
-    #
-    # Filter for lysine only
-    #
-    lysine_filter = args['--lys']
-
-    try:
-        lysine_filter = int(lysine_filter)
-        if lysine_filter not in [1, 2]:
-            lysine_filter = 0
-
-    except TypeError or ValueError:
-        lysine_filter = 0
-
-    print(lysine_filter)
-
-
-    #
-    # Convert the to-do isotopomer list option into a list of integers
-    #
-    iso_to_do = []
-    for char in args['--iso'].split(','):
-
-        try:
-            char = int(char)
-
-            # Only doing down to mz + 12.
-            if char <= 12:
-                iso_to_do.append(int(char))
-
-        except ValueError or TypeError:
-            pass
-
-    if not iso_to_do:
-        sys.exit('Error: Invalid isotopomer list given.')
-
-    iso_to_do = list(set(iso_to_do))
-    iso_to_do.sort()
-
-    print('Recognized isotopomer list:', str(iso_to_do))
-
-    #
-    # Percolator q value cutoff for peptides and proteins
-    #
-    if args['--qvalue']:
-
-        try:
-            qcutoff = float(args['--qvalue'])
-
-        except ValueError or TypeError:
-            print('Invalid Q value given - using default value.')
-            qcutoff = float(1e-2)
-
-    #
-    # Retention time cutoff peptides and proteins
-    #
-    if args['--rt']:
-
-        try:
-            rt_tolerance = float(args['--rt'])
-
-        except ValueError or TypeError:
-            print('Invalid retention time tolerance given - using default value.')
-            rt_tolerance = float(1.0)
-    else:
-        rt_tolerance = float(1.0)
-
-    #
-    # Open the mzid file. Note that take_psm_df_cvParams should be set to False to avoid conflict
-    # between the identical "Percolator:Q value" cvParam names inside both PSM and Peptide fields.
-    #
-    try:
-        mzid = Mzid(mzid_loc)
-        mzid.make_peptide_summary(take_psm_df_cvParams=False)
-        mzid.filter_peptide_summary(lysine_filter=lysine_filter,
-                                    protein_q=qcutoff,
-                                    peptide_q=1,
-                                    unique_only=unique_pep,
-                                    require_protein_id=True)
-
-    except OSError as e:
-        sys.exit('Failed to load mzid file. ' + str(e.errno))
-
-    #
-    # Filter only for peptides with percolator posterior error probability below cutoff
-    #
-
-    #
-    # Filter only for peptides in a protein
-    #
-    #a.pep_summary_df = a.pep_summary_df.loc[lambda x: x.acc.astype(str) != "nan", :]
-
-
-    #
-    # Read the mzml files
-    #
-    try:
-        mzml = Mzml(mzml_loc)
-    except OSError as e:
-        sys.exit('Failed to load mzml file. ' + str(e.errno))
-
-    #
-    # Determine how many peptides to integrate. If test_run is on, only do 5 peptides to save time.
-    #
-    end = len(mzid.filtered_pep_summary_df)
-    if test_run:
-        end = min(5, len(mzid.filtered_pep_summary_df))
-
-    if end == 0:
-        return sys.exit(os.EX_CONFIG)
-
-    print(mzid.filtered_pep_summary_df)
-
-    print(end)
-    #
-    # Main loop through the mzidentml summary to pass tasks to the integration worker
-    #
-
-
-    #
-    # Prepare an empty list to encompass the output from each row of the mzidentml summary
-    #
-    output_table = [[] for i in range(0, end)]
-
-    for i in range(end):
-
-        out = []
-
-
-        print('Integrating peptide ' + str(i) + ' of ' + str(end))
-
-        rt = mzml.get_rt_from_scan(float(mzid.filtered_pep_summary_df.loc[i, 'spectrum_id']))
-        z = float(mzid.filtered_pep_summary_df.loc[i, 'z'])
-
-        iso = mzml.get_isotopes_from_amrt(peptide_am=float(mzid.filtered_pep_summary_df.loc[i, 'calc_mz']),
-                                          peptide_rt=rt,
-                                          z=z,
-                                          rt_tolerance=rt_tolerance,
-                                          iso_to_do=iso_to_do)
-
-        if iso:
-            out.append(i)
-            out.append(mzid.filtered_pep_summary_df.loc[i, 'acc'])
-            out.append(mzid.filtered_pep_summary_df.loc[i, 'seq'])
-            out.append(mzid.filtered_pep_summary_df.loc[i, 'z'])
-            out.append(rt)
-            out.append(mzid.filtered_pep_summary_df.loc[i, 'calc_mz'])
-
-            for j in range(0, len(iso_to_do)):
-                out.append(iso[j][1])
-
-            print(out)
-
-            output_table[i] = out
-
-
-
-    #
-    # Convert the output_table into a data frame
-    #
-    df_columns = ['ID', 'acc', 'peptide', 'z', 'rt', 'calc_mz',]
-
-    for each_iso in iso_to_do:
-        df_columns.append('m' + str(each_iso))
-
-    out_df = pd.DataFrame(output_table, columns=df_columns)
-
-    out_df.to_csv(out_loc, sep='\t')
-
-    return sys.exit(os.EX_OK)
-
-
-
-def integrate_fast(args):
-    """
-    Second try: improve the process to integrate for isotope abundance analysis.
+    Improved process to integrate for isotope abundance analysis.
     Idea is to loop through the mzML only once - get all the peptides to be integrated first
-    Find all the spectrum ID - m/z combinations to be integrated, then integrate them sequentialy
+    Find all the spectrum ID - m/z combinations to be integrated, then integrate them sequentially
 
     :param args: Arguments from command line
     :return:
@@ -248,18 +36,18 @@ def integrate_fast(args):
     """
 
     # Handle command line arguments
-    mzid_loc = args['<mzid>']
-    mzml_loc = args['<mzml>']
+    mzid_loc = args.mzid
+    mzml_loc = args.mzml
 
     # Handle command line options
-    out_loc = args['--out']
-    test_run = args['--test']
-    unique_pep = args['--unique']
+    out_loc = args.out
+    test_run = args.test
+    unique_pep = args.unique
 
     #
     # Filter for lysine only
     #
-    lysine_filter = args['--lys']
+    lysine_filter = args.lysine
 
     try:
         lysine_filter = int(lysine_filter)
@@ -276,7 +64,7 @@ def integrate_fast(args):
     # Convert the to-do isotopomer list option into a list of integers
     #
     iso_to_do = []
-    for char in args['--iso'].split(','):
+    for char in args.iso.split(','):
 
         try:
             char = int(char)
@@ -299,10 +87,10 @@ def integrate_fast(args):
     #
     # Percolator q value cutoff for peptides and proteins
     #
-    if args['--qvalue']:
+    if args.qvalue:
 
         try:
-            qcutoff = float(args['--qvalue'])
+            qcutoff = float(args.qvalue)
 
         except ValueError or TypeError:
             print('Invalid Q value given - using default value.')
@@ -311,10 +99,10 @@ def integrate_fast(args):
     #
     # Retention time cutoff peptides and proteins
     #
-    if args['--rt']:
+    if args.rtime:
 
         try:
-            rt_tolerance = float(args['--rt'])
+            rt_tolerance = float(args.rtime)
 
         except ValueError or TypeError:
             print('Invalid retention time tolerance given - using default value.')
@@ -500,17 +288,51 @@ def integrate_fast(args):
 
     return sys.exit(os.EX_OK)
 
-#
-# Docopt
-#
-if __name__ == "__main__":
-    args = docopt(__doc__, version='Relative Isotope Analyser v.0.2.0')
-    print(args)
-    if args['integrate']:
-        integrate(args)
-    elif args['integrate_fast']:
-        integrate_fast(args)
-    elif args['merge']:
-        merge(args)
 
+#
+# Code for running main with parsed arguments from command line
+#
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description='RIAna v.0.3.0 integrates the relative abundance of'
+                                                 'isotopomers')
+
+
+    # Create a "translate_rmats" subparser and house its specific arguments
+    parser.add_argument('mzid', help='path to mzid file')
+    parser.add_argument('mzml', help='path to mzml file')
+
+    parser.add_argument('-i', '--iso', help='isotopes to do, separated by commas, e.g., 0,1,2,3,4,5 [default: 0,6]',
+                              default='0,6')
+    parser.add_argument('-u', '--unique', action='store_true', help='integrate unique peptides only')
+    parser.add_argument('-k', '--lysine',
+                        help='lysine mode, 0=do not filter, 1=only one lysine, 2=any lysine [default = 0',
+                        choices=[0,1,2],
+                        default=0)
+    parser.add_argument('-t', '--test', action='store_true',
+                        help='test mode: integrates only first 5 qualifying peptides')
+    parser.add_argument('-q', '--qvalue', help='integrate only peptides with q value below this threshold[default: 1e-2]',
+                        default=1e-2)
+    parser.add_argument('-r' '--rtime', help='retention time (in minutes, both directions) tolerance for integration',
+                        default=1e-2)
+    parser.add_argument('-o', '--out', help='name of the output files [default: ria.txt]',
+                        default='ria.txt')
+    parser.add_argument('-v', '--verbose', action='store_true', help='verbose error messages')
+
+
+    parser.set_defaults(func=integrate)
+
+    # Print help message if no arguments are given
+    import sys
+    if len(sys.argv[1:]) == 0:
+        parser.print_help()
+        parser.exit()
+
+    # Parse all the arguments
+    args = parser.parse_args()
+
+    # Run the function in the argument
+    args.func(args)
 
