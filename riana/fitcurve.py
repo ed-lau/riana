928dc688d9277806b003c92add1e252668c496fe
Edward Lau <edward.lau@me.com>
2021-11-08 21:19:39 -0700

    adding fit function

---
+++ riana/fitcurve.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+""" Main. """
+import logging
+import re
+import os
+import sys
+from functools import partial
+
+from riana.project import ReadDirectory
+from riana.peptides import ReadPercolator
+from riana.spectra import Mzml
+
+from riana import integrate, params, __version__
+
+import tqdm
+import pandas as pd
+
+def runfit(args):
+    print('Fit function not implemented yet.')
+    pass
