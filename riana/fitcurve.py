65565a19caa2145fc6f53320442b7c36bebc101b
Edward Lau <edward.lau@me.com>
2021-11-10 10:31:45 -0700

    fitting functions

--- riana/fitcurve.py
+++ riana/fitcurve.py
@@ -4,20 +4,15 @@
 import logging
 import re
 import os
-import sys
-from functools import partial
-
-from riana.project import ReadDirectory
-from riana.peptides import ReadPercolator
-from riana.spectra import Mzml
-
-from riana import integrate, params, __version__
 
 import tqdm
 import pandas as pd
-
 import numpy as np
-from riana import accmass, constants
+from scipy import optimize
+from functools import partial
+import matplotlib.pyplot as plt
+
+from riana import accmass, constants, models, __version__
 
 
 def strip_concat(sequence: str,
@@ -79,15 +74,44 @@ def fit_all(args):
     :return:
     """
 
+
+
+    # Parse arguments
+
     riana_list = args.riana_path
 
-    model = args.model # 'simple'
+    model = args.model  # 'simple'
     q_threshold = args.q_value
     t_threshold = args.depth
     ria_max = 0.047
     outdir = 'out/snakemake'
     if not os.path.exists(outdir):
         os.makedirs(outdir)
+
+
+    # Logging
+    fit_log = logging.getLogger('riana.fit')
+    fit_log.setLevel(logging.DEBUG)
+
+    # create file handler which logs even debug messages
+    fh = logging.FileHandler(os.path.join(outdir, 'riana_fit.log'))
+    fh.setLevel(logging.INFO)
+
+    # create console handler with a higher log level
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.ERROR)
+
+    # create formatter and add it to the handlers
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    ch.setFormatter(formatter)
+
+    # add the handlers to the logger
+    fit_log.addHandler(fh)
+    fit_log.addHandler(ch)
+
+    fit_log.info(args)
+    fit_log.info(__version__)
 
     # %%
 
@@ -105,9 +129,6 @@ def fit_all(args):
     concats = concats[concats >= t_threshold]
     rdf_filtered = rdf_filtered[rdf_filtered.concat.isin(concats.index)]
 
-    # rdf_filtered = rdf_filtered[rdf_filtered['concat'] in concats.index]
-    print(rdf_filtered.sort_values(by='concat').concat)
-
     # output dictionary
     out_dict = {}
 
@@ -118,43 +139,41 @@ def fit_all(args):
 
         # TODO: this is fit one
 
-        print(seq)
-        print(y.shape)
+        fit_log.info(f'Fitting peptide {seq} with data shape {y.shape}')
 
         if y.shape[0] < t_threshold:
             continue
 
         y['mi'] = y['m0'] / (y['m0'] + y['m1'] + y['m2'] + y['m3'] + y['m4'] + y['m5'])
-        print(y[['sample', 'mi']])
+
+        fit_log.info(y[['sample', 'mi']])
 
         stripped = strip_concat(seq)
         num_labeling_sites = calculate_label_n(seq)
         a_0 = calculate_a0(seq)
         a_max = a_0 * np.power((1 - ria_max), num_labeling_sites)
 
-        print(stripped, num_labeling_sites, a_0, a_max)
-        continue
-        # test = y[['sample', 'mi']]
+        fit_log.info(f'concat: {stripped}, n: {num_labeling_sites}, a_0: {a_0}, a_max: {a_max}')
 
         t = np.array([float(re.sub('[^0-9]', '', time)) for time in y['sample']])
         mi = np.array(y['mi'].tolist())
 
-        fs = riana.fitcurve.calculate_fs(a=mi, a_0=a_0, a_max=a_max)
+        fs = calculate_fs(a=mi, a_0=a_0, a_max=a_max)
 
-        print(fs)
-        print(mi)
+        fit_log.info(fs)
 
-        popt, pcov = optimize.curve_fit(f=partial(riana.models.one_exponent, a_0=0., a_max=1.),
+        # Optimization
+        popt, pcov = optimize.curve_fit(f=partial(models.one_exponent, a_0=0., a_max=1.),
                                         xdata=t,
                                         ydata=fs,
                                         bounds=([0], [10]),
                                         )
-
         print(popt)
         print(popt[0])
         print(np.sqrt(np.diag(pcov)))
 
-        residuals = fs - riana.models.one_exponent(t, a_max=1., a_0=0., k_deg=popt[0])
+
+        residuals = fs - models.one_exponent(t, a_max=1., a_0=0., k_deg=popt[0])
         ss_res = np.sum(residuals ** 2)
 
         ss_tot = np.sum((fs - np.mean(fs)) ** 2)
@@ -163,20 +182,21 @@ def fit_all(args):
         sd = np.sqrt(np.diag(pcov))[0]
         print(residuals, r_squared)
 
+
         fig, ax = plt.subplots()
         plt.plot(t, fs, '.', label='Fractional synthesis')
         plt.plot(np.array(range(0, 31)),
-                 riana.models.one_exponent(t=np.array(range(0, 31)), k_deg=popt, a_0=0, a_max=1),
+                 models.one_exponent(t=np.array(range(0, 31)), k_deg=popt, a_0=0, a_max=1),
                  'r-', label=f'k_deg={np.round(popt[0], 3)}'
                  )
 
         plt.plot(np.array(range(0, 31)),
-                 riana.models.one_exponent(t=np.array(range(0, 31)), k_deg=popt[0] + sd, a_0=0, a_max=1),
+                 models.one_exponent(t=np.array(range(0, 31)), k_deg=popt[0] + sd, a_0=0, a_max=1),
                  'r--', label=f'sd={sd}'
                  )
 
         plt.plot(np.array(range(0, 31)),
-                 riana.models.one_exponent(t=np.array(range(0, 31)), k_deg=popt[0] ** 2 / (popt[0] + sd), a_0=0,
+                 models.one_exponent(t=np.array(range(0, 31)), k_deg=popt[0] ** 2 / (popt[0] + sd), a_0=0,
                                            a_max=1),
                  'r--', label=f'sd={sd}'
                  )
@@ -187,13 +207,16 @@ def fit_all(args):
         plt.xlim([-1, 32])
         plt.ylim([-0.1, 1.1])
         # plt.show()
-        fig.savefig(f'out/{seq}.png')
+
+        plot_dir = os.path.join(outdir, 'curves')
+        if not os.path.exists(plot_dir):
+            os.makedirs(plot_dir)
+        fig.savefig(os.path.join(plot_dir, f'{seq}.png'))
 
         out_dict[seq] = [popt[0], r_squared, sd]
 
-    # %%
-    # out_df = pd.DataFrame.from_dict(out_dict, orient='index', columns=['k_deg', 'R_squared', 'sd'])
+    out_df = pd.DataFrame.from_dict(out_dict, orient='index', columns=['k_deg', 'R_squared', 'sd'])
 
-    # out_df.to_csv("out/out.csv")
+    out_df.to_csv(os.path.join(outdir, 'riana_fit_peptides.tsv'))
 
     return True
