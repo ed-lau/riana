935f913b32a1ea30ea8c5a7fdc7dc576b34c0425
Edward Lau <edward.lau@me.com>
2021-11-10 11:24:52 -0700

    fitting functions

--- riana/fitcurve.py
+++ riana/fitcurve.py
@@ -54,7 +54,7 @@ def calculate_a0(sequence: str,
     return a0
 
 
-def calculate_label_n(sequence:str,
+def calculate_label_n(sequence: str,
                       ) -> float:
 
     sequence = strip_concat(sequence)
@@ -68,28 +68,30 @@ def calculate_fs(a, a_0, a_max):
 
 def fit_all(args):
     """
-    fits all concatamers
+    performs kinetic curve-fitting of integration output using a kinetics model
+    returns a csv file containing the peptide concatamer, k_deg, sd, and r2
 
     :param args:
     :return:
     """
 
+    #
+    # parse arguments
+    #
 
+    riana_list = args.riana_path        # list of integration output
+    model = args.model                  # kinetic model, e.g., 'simple'
+    q_threshold = args.q_value          # peptide q value threshold
+    t_threshold = args.depth            # minimal number of time points threshold
+    ria_max = args.ria                  # final isotope enrichment level (e.g., 0.046)
+    outdir = args.out                   # output directory
 
-    # Parse arguments
-
-    riana_list = args.riana_path
-
-    model = args.model  # 'simple'
-    q_threshold = args.q_value
-    t_threshold = args.depth
-    ria_max = 0.047
-    outdir = 'out/snakemake'
     if not os.path.exists(outdir):
         os.makedirs(outdir)
 
-
-    # Logging
+    #
+    # logging
+    #
     fit_log = logging.getLogger('riana.fit')
     fit_log.setLevel(logging.DEBUG)
 
@@ -109,17 +111,15 @@ def fit_all(args):
     # add the handlers to the logger
     fit_log.addHandler(fh)
     fit_log.addHandler(ch)
-
     fit_log.info(args)
     fit_log.info(__version__)
 
-    # %%
-
-    # open the riana files
+    #
+    # read the integration output files in
+    #
     rdf = pd.DataFrame()
     for in_file in riana_list:
         rdf = rdf.append(pd.read_table(in_file))
-
 
     # filter by percolator q-value
     rdf_filtered = rdf[rdf['percolator q-value'] < q_threshold]
@@ -129,76 +129,68 @@ def fit_all(args):
     concats = concats[concats >= t_threshold]
     rdf_filtered = rdf_filtered[rdf_filtered.concat.isin(concats.index)]
 
-    # output dictionary
+    # create output dictionary
     out_dict = {}
 
-    # Loop through each qualifying peptide
+    # loop through each qualifying peptide
     for seq in tqdm.tqdm(rdf_filtered.concat.unique()):
 
+        # create subset dataframe with current concat
         y = rdf_filtered.loc[rdf_filtered['concat'] == seq].copy()
-
-        # TODO: this is fit one
-
         fit_log.info(f'Fitting peptide {seq} with data shape {y.shape}')
 
-        if y.shape[0] < t_threshold:
-            continue
-
+        # get t, mi from subset dataframe
         y['mi'] = y['m0'] / (y['m0'] + y['m1'] + y['m2'] + y['m3'] + y['m4'] + y['m5'])
-
         fit_log.info(y[['sample', 'mi']])
+        t = np.array([float(re.sub('[^0-9]', '', time)) for time in y['sample']])
+        mi = np.array(y['mi'].tolist())
 
+        # calculate a_0, a_max, and fractional synthesis
         stripped = strip_concat(seq)
         num_labeling_sites = calculate_label_n(seq)
         a_0 = calculate_a0(seq)
         a_max = a_0 * np.power((1 - ria_max), num_labeling_sites)
-
-        fit_log.info(f'concat: {stripped}, n: {num_labeling_sites}, a_0: {a_0}, a_max: {a_max}')
-
-        t = np.array([float(re.sub('[^0-9]', '', time)) for time in y['sample']])
-        mi = np.array(y['mi'].tolist())
-
         fs = calculate_fs(a=mi, a_0=a_0, a_max=a_max)
 
-        fit_log.info(fs)
+        fit_log.info(f'concat: {stripped}, n: {num_labeling_sites}, a_0: {a_0}, a_max: {a_max}')
+        fit_log.info([t, fs])
 
-        # Optimization
+        # perform curve-fitting
         popt, pcov = optimize.curve_fit(f=partial(models.one_exponent, a_0=0., a_max=1.),
                                         xdata=t,
                                         ydata=fs,
                                         bounds=([0], [10]),
                                         )
-        print(popt)
-        print(popt[0])
-        print(np.sqrt(np.diag(pcov)))
+        # calculate standard deviation from the non-linear least square cov
+        sd = np.sqrt(np.diag(pcov))[0]
 
-
+        # calculate residuals and r2
         residuals = fs - models.one_exponent(t, a_max=1., a_0=0., k_deg=popt[0])
         ss_res = np.sum(residuals ** 2)
-
         ss_tot = np.sum((fs - np.mean(fs)) ** 2)
         r_squared = 1 - (ss_res / ss_tot)
+        fit_log.info(f'Best fit k_deg: {popt[0]}, sd: {sd}, residuals: {residuals}, R2: {r_squared}')
 
-        sd = np.sqrt(np.diag(pcov))[0]
-        print(residuals, r_squared)
-
-
+        # create plot
         fig, ax = plt.subplots()
         plt.plot(t, fs, '.', label='Fractional synthesis')
         plt.plot(np.array(range(0, 31)),
-                 models.one_exponent(t=np.array(range(0, 31)), k_deg=popt, a_0=0, a_max=1),
+                 models.one_exponent(t=np.array(range(0, int(np.max(t)))), k_deg=popt, a_0=0, a_max=1),
                  'r-', label=f'k_deg={np.round(popt[0], 3)}'
                  )
 
         plt.plot(np.array(range(0, 31)),
-                 models.one_exponent(t=np.array(range(0, 31)), k_deg=popt[0] + sd, a_0=0, a_max=1),
-                 'r--', label=f'sd={sd}'
+                 models.one_exponent(t=np.array(range(0, int(np.max(t)))), k_deg=popt[0] + sd, a_0=0, a_max=1),
+                 'r--', label=f'Upper={np.round(popt[0]+sd, 3)}'
                  )
 
         plt.plot(np.array(range(0, 31)),
-                 models.one_exponent(t=np.array(range(0, 31)), k_deg=popt[0] ** 2 / (popt[0] + sd), a_0=0,
-                                           a_max=1),
-                 'r--', label=f'sd={sd}'
+                 models.one_exponent(t=np.array(range(0, int(np.max(t)))),
+                                     k_deg=popt[0] ** 2 / (popt[0] + sd),
+                                     a_0=0,
+                                     a_max=1,
+                                     ),
+                 'r--', label=f'Lower={np.round(popt[0] ** 2 / (popt[0] + sd), 3)}'
                  )
         plt.xlabel('t')
         plt.ylabel('fs')
@@ -212,6 +204,7 @@ def fit_all(args):
         if not os.path.exists(plot_dir):
             os.makedirs(plot_dir)
         fig.savefig(os.path.join(plot_dir, f'{seq}.png'))
+        plt.close(fig)
 
         out_dict[seq] = [popt[0], r_squared, sd]
 
