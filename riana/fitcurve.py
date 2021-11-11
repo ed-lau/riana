912627bd131e9dbb0bb5ae17d1a0e2a570530b42
Edward Lau <edward.lau@me.com>
2021-11-10 20:40:39 -0700

    parallelization for fitting

--- riana/fitcurve.py
+++ riana/fitcurve.py
@@ -90,6 +90,16 @@ def fit_all(args):
     if not os.path.exists(outdir):
         os.makedirs(outdir)
 
+    if args.thread:
+        try:
+            num_threads = max(os.cpu_count() * 4, int(args.thread))
+
+        except ValueError or TypeError:
+            num_threads = 1  # os.cpu_count() * 4
+
+    else:
+        num_threads = 1  # os.cpu_count() * 4
+
     #
     # logging
     #
@@ -133,66 +143,59 @@ def fit_all(args):
     # create output dictionary
     out_dict = {}
 
-    # loop through each qualifying peptide
-    for seq in tqdm.tqdm(rdf_filtered.concat.unique(),
-                         desc=f'Fitting Curves'):
+    loop_ = range(len(rdf_filtered.concat.unique()))
 
-        # create subset dataframe with current concat
-        y = rdf_filtered.loc[rdf_filtered['concat'] == seq].copy()
-        fit_log.info(f'Fitting peptide {seq} with data shape {y.shape}')
+    fit_one_partial = partial(fit_one,
+                              concat_list=rdf_filtered.concat.unique(),
+                              filtered_integrated_df=rdf_filtered.copy(),
+                              ria_max=ria_max,
+                              )
 
-        # get t, mi from subset dataframe
-        y['mi'] = y['m0'] / (y['m0'] + y['m1'] + y['m2'] + y['m3'] + y['m4'] + y['m5'])
-        fit_log.info(y[['sample', 'mi']])
-        t = np.array([float(re.sub('[^0-9]', '', time)) for time in y['sample']])
-        mi = np.array(y['mi'].tolist())
+    # Single threaded loop
+    # '''
+    # results = []
+    # for i in loop_:
+    #     print(i)
+    #     results += fit_one_partial(i)
+    # '''
 
-        # calculate a_0, a_max, and fractional synthesis
-        stripped = strip_concat(seq)
-        num_labeling_sites = calculate_label_n(seq)
-        a_0 = calculate_a0(seq)
-        a_max = a_0 * np.power((1 - ria_max), num_labeling_sites)
-        fs = calculate_fs(a=mi, a_0=a_0, a_max=a_max)
+    # For parallelism, use concurrent.futures instead of multiprocessing for higher speed
+    # '''
+    from concurrent import futures
+    with futures.ThreadPoolExecutor(max_workers=num_threads) as ex:
+        results = list(tqdm.tqdm(ex.map(fit_one_partial, loop_),
+                                 total=max(loop_),
+                                 desc=f'Fitting peptides to {model} model:'))
+    # '''
 
-        fit_log.info(f'concat: {stripped}, n: {num_labeling_sites}, a_0: {a_0}, a_max: {a_max}')
-        fit_log.info([t, fs])
+    # collect all the dicts, plot out graphs and output final file
+    out_dict = {}
 
-        # perform curve-fitting
-        popt, pcov = optimize.curve_fit(f=partial(models.one_exponent, a_0=0., a_max=1.),
-                                        xdata=t,
-                                        ydata=fs,
-                                        bounds=([0], [10]),
-                                        )
-        # calculate standard deviation from the non-linear least square cov
-        sd = np.sqrt(np.diag(pcov))[0]
+    for res in tqdm.tqdm(results, desc=f'Plotting curves'):
 
-        # calculate residuals and r2
-        residuals = fs - models.one_exponent(t, a_max=1., a_0=0., k_deg=popt[0])
-        ss_res = np.sum(residuals ** 2)
-        ss_tot = np.sum((fs - np.mean(fs)) ** 2)
-        r_squared = 1 - (ss_res / ss_tot)
-        fit_log.info(f'Best fit k_deg: {popt[0]}, sd: {sd}, residuals: {residuals}, R2: {r_squared}')
+        seq = list(res.keys())[0]
+        k_deg, r_squared, sd, t, fs = list(res.values())[0]
 
         # create plot
         fig, ax = plt.subplots()
         plt.plot(t, fs, '.', label='Fractional synthesis')
         plt.plot(np.array(range(0, 31)),
-                 models.one_exponent(t=np.array(range(0, int(np.max(t)))), k_deg=popt, a_0=0, a_max=1),
-                 'r-', label=f'k_deg={np.round(popt[0], 3)}'
+                 models.one_exponent(t=np.array(range(0, int(np.max(t)))), k_deg=k_deg, a_0=0, a_max=1),
+                 'r-', label=f'k_deg={np.round(k_deg, 3)}'
                  )
 
         plt.plot(np.array(range(0, 31)),
-                 models.one_exponent(t=np.array(range(0, int(np.max(t)))), k_deg=popt[0] + sd, a_0=0, a_max=1),
-                 'r--', label=f'Upper={np.round(popt[0]+sd, 3)}'
+                 models.one_exponent(t=np.array(range(0, int(np.max(t)))), k_deg=k_deg + sd, a_0=0, a_max=1),
+                 'r--', label=f'Upper={np.round(k_deg + sd, 3)}'
                  )
 
         plt.plot(np.array(range(0, 31)),
                  models.one_exponent(t=np.array(range(0, int(np.max(t)))),
-                                     k_deg=popt[0] ** 2 / (popt[0] + sd),
+                                     k_deg=k_deg ** 2 / (k_deg + sd),
                                      a_0=0,
                                      a_max=1,
                                      ),
-                 'r--', label=f'Lower={np.round(popt[0] ** 2 / (popt[0] + sd), 3)}'
+                 'r--', label=f'Lower={np.round(k_deg ** 2 / (k_deg + sd), 3)}'
                  )
         plt.xlabel('t')
         plt.ylabel('fs')
@@ -208,10 +211,66 @@ def fit_all(args):
         fig.savefig(os.path.join(plot_dir, f'{seq}.png'))
         plt.close(fig)
 
-        out_dict[seq] = [popt[0], r_squared, sd]
+        out_dict = out_dict | res
 
-    out_df = pd.DataFrame.from_dict(out_dict, orient='index', columns=['k_deg', 'R_squared', 'sd'])
+    out_df = pd.DataFrame.from_dict(out_dict, orient='index', columns=['k_deg', 'R_squared', 'sd', 't', 'fs'])
 
     out_df.to_csv(os.path.join(outdir, 'riana_fit_peptides.csv'))
 
     return sys.exit(os.EX_OK)
+
+
+def fit_one(loop_index,
+            concat_list: list,
+            filtered_integrated_df: pd.DataFrame,
+            ria_max: float,
+            ):
+    """
+
+    :param loop_index:
+    :param concat_list:
+    :param filtered_integrated_df:
+    :param ria_max:
+    :return:
+    """
+
+    # create subset dataframe with current concat
+    seq = concat_list[loop_index]
+    y = filtered_integrated_df.loc[filtered_integrated_df['concat'] == seq].copy()
+
+    fit_log = logging.getLogger('riana.fit')
+    fit_log.info(f'Fitting peptide {seq} with data shape {y.shape}')
+
+    # get t, mi from subset dataframe
+    y['mi'] = y['m0'] / (y['m0'] + y['m1'] + y['m2'] + y['m3'] + y['m4'] + y['m5'])
+    fit_log.info(y[['sample', 'mi']])
+    t = np.array([float(re.sub('[^0-9]', '', time)) for time in y['sample']])
+    mi = np.array(y['mi'].tolist())
+
+    # calculate a_0, a_max, and fractional synthesis
+    stripped = strip_concat(seq)
+    num_labeling_sites = calculate_label_n(seq)
+    a_0 = calculate_a0(seq)
+    a_max = a_0 * np.power((1 - ria_max), num_labeling_sites)
+    fs = calculate_fs(a=mi, a_0=a_0, a_max=a_max)
+
+    fit_log.info(f'concat: {stripped}, n: {num_labeling_sites}, a_0: {a_0}, a_max: {a_max}')
+    fit_log.info([t, fs])
+
+    # perform curve-fitting
+    popt, pcov = optimize.curve_fit(f=partial(models.one_exponent, a_0=0., a_max=1.),
+                                    xdata=t,
+                                    ydata=fs,
+                                    bounds=([0], [10]),
+                                    )
+    # calculate standard deviation from the non-linear least square cov
+    sd = np.sqrt(np.diag(pcov))[0]
+
+    # calculate residuals and r2
+    residuals = fs - models.one_exponent(t, a_max=1., a_0=0., k_deg=popt[0])
+    ss_res = np.sum(residuals ** 2)
+    ss_tot = np.sum((fs - np.mean(fs)) ** 2)
+    r_squared = 1 - (ss_res / ss_tot)
+    fit_log.info(f'Best fit k_deg: {popt[0]}, sd: {sd}, residuals: {residuals}, R2: {r_squared}')
+
+    return {seq: [popt[0], r_squared, sd, t, fs]}
